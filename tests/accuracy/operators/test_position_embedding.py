1ad93713fde7f0738925eed7b2d700862258b2b5
minicybersheep <minicybersheep@outlook.com>
2026-01-29 23:27:33 +0800

    修改测试用例
    (cherry picked from commit 0a8b9e4e5f2e81e3d7ec1d8cbde069d3a07ae905)
    
    .

--- tests/accuracy/operators/test_position_embedding.py
+++ tests/accuracy/operators/test_position_embedding.py
@@ -2,7 +2,7 @@ import pytest
 import torch
 import os
 
-from tests.utils import bypass_not_implemented
+from tests.utils import auto_switch_platform, bypass_not_implemented
 
 from mojo_opset import MojoRoPE
 from mojo_opset import MojoIndexerRoPE
